0bbba62696f1af788c81ed30fb0f04e969bac3b2
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-06 12:44:22 +0200

    Add project files.

---
+++ VowelPalindromes/FormVowelPalindromes.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VowelPalindromes
+{
+    partial class FormVowelPalindromes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(52, 53);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(177, 61);
+            this.BtnCheck.TabIndex = 0;
+            this.BtnCheck.Text = "Check for Vowel Palindromes";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(261, 53);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(479, 356);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormVowelPalindromes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheck);
+            this.Name = "FormVowelPalindromes";
+            this.Text = "Vowel Palindromes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ VowelPalindromes/FormVowelPalindromes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VowelPalindromes
+{
+    public partial class FormVowelPalindromes : Form
+    {
+        public FormVowelPalindromes()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            string letter;
+
+            string path = Application.StartupPath + @"\vowels.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    string vowels = "";
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        letter = line.Substring(i, 1);
+
+                        if (letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U" || letter == "Y")
+                        {
+                            vowels += letter;
+                        }
+                    }
+                    TxtResult.Text += vowels + " ";
+                    TxtResult.Text += CheckForVowelPalindromes(vowels) ? " IS PALINDROME" : " IS NOT A PALINDROME";
+                    TxtResult.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private bool CheckForVowelPalindromes(string word)
+        {
+            //We compare the first letter with the last and we do the same for all the rest letters of the word.
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word.Substring(i, 1) != word.Substring(word.Length - (i + 1), 1))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
